91e7e967a0e48cbd29fcf76aa953b4fc8a55f8b8
Aleh Khvoshch <akhvoshch@edmunds.com>
2016-11-17 18:14:20 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var element = React.createElement(
+    'h1',
+    null
+);
+
+ReactDOM.render(element, document.getElementById('bodyid'));
